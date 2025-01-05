12523e149ca9202b1ad225bec496cf2d36a5f304
Vansh <vanshayushi@gmail.com>
2025-01-05 13:07:54 +0530

    update clients and favicon

--- src/Components/Clients/Clients.jsx
+++ src/Components/Clients/Clients.jsx
@@ -4,6 +4,8 @@ import logo2 from "../../Assets/12-client-2.png"
 import logo3 from "../../Assets/12-client-3.png"
 import logo4 from "../../Assets/12-client-4.png"
 import logo5 from "../../Assets/12-client-5.png"
+import logo6 from "../../Assets/01-client-6.png"
+import logo7 from "../../Assets/01-client-7.png"
 
 const Clients = () => {
     const clients = [
@@ -11,7 +13,10 @@ const Clients = () => {
         { name: 'Retro People', logo: logo2 },
         { name: 'Emma Bradberry', logo: logo3 },
         { name: 'Rebecca Photography', logo: logo4 },
-        { name: 'Anna Grey Florist', logo: logo5 }
+        { name: 'Anna Grey Florist', logo: logo5 },
+        // { name: 'Sammy Florist', logo: logo6 },
+        { name: 'Monk Videography', logo: logo7 }
+
     ];
 
     const [currentIndex, setCurrentIndex] = useState(0);
@@ -31,7 +36,7 @@ const Clients = () => {
     }, [clients.length]);
 
     return (
-        <div className="py-20 bg-white">
+        <div className="py-40 bg-white">
             <div className="max-w-6xl mx-auto px-4">
                 <div className="text-center mb-16">
                     <h2 className="text-4xl text-gray-700 mb-6">OUR CLIENTS</h2>
@@ -40,7 +45,7 @@ const Clients = () => {
                         <div className="w-2 h-2 rounded-full bg-gray-300"></div>
                         <div className="h-px w-12 bg-gray-300"></div>
                     </div>
-                    <p className="text-gray-500 italic max-w-3xl mx-auto">
+                    <p className="text-gray-500 italic max-w-5xl mx-auto">
                         Lorem ipsum dolor sit amet, consectetuer gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auci elit consequat ipsutis sem nibis sed odio sit amet nibh
                     </p>
                 </div>
@@ -52,12 +57,12 @@ const Clients = () => {
                         {clients.slice(currentIndex, currentIndex + 5).map((client, index) => (
                             <div
                                 key={`${client.name}-${index}`}
-                                className="w-[160px] flex items-center justify-center"
+                                className="w-[200px] flex items-center justify-center"
                             >
                                 <img
                                     src={client.logo}
                                     alt={client.name}
-                                    className="w-full h-auto opacity-50 hover:opacity-100 transition-opacity duration-300"
+                                    className="w- h-auto opacity-50 hover:opacity-100 transition-opacity duration-300"
                                 />
                             </div>
                         ))}
